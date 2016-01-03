c7f87fb941fd639fadf2f3598a3ac8923378a2a4
Mikhail Yakshin <greycat@altlinux.org>
2016-01-03 23:00:06 +0300

    Reworked YAML file structure to be lazy, classy and load-on-demand

--- kaitai_structures.rb
+++ kaitai_structures.rb
@@ -1,24 +1,24 @@
 module KaitaiStructures
   def ensure_fixed_contents(size, expected)
-    actual = @f.read(size).bytes
+    actual = @_io.read(size).bytes
     if actual != expected
       raise "Unexpected fixed contents: got #{actual.inspect}, was waiting for #{expected.inspect}"
     end
   end
 
   def read_u1
-    @f.read(1).unpack('C')[0]
+    @_io.read(1).unpack('C')[0]
   end
 
   def read_u4le
-    @f.read(4).unpack('N')[0]
+    @_io.read(4).unpack('N')[0]
   end
 
   def read_u8le
-    @f.read(8).unpack('Q')[0]
+    @_io.read(8).unpack('Q')[0]
   end
 
   def read_u4be
-    @f.read(4).unpack('V')[0]
+    @_io.read(4).unpack('V')[0]
   end
 end
