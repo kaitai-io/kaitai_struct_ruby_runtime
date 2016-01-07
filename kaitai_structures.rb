98f7d919938bb2428fdbef8675c6e1f09bdd5b5a
Mikhail Yakshin <greycat@altlinux.org>
2016-01-08 02:00:40 +0300

    Implemented various endianness handling

--- kaitai_structures.rb
+++ kaitai_structures.rb
@@ -1,4 +1,7 @@
 module KaitaiStructures
+  # Test endianness of the platform
+  @@big_endian = [0x0102].pack('s') == [0x0102].pack('n')
+
   def ensure_fixed_contents(size, expected)
     actual = @_io.read(size).bytes
     if actual != expected
@@ -22,8 +25,15 @@ module KaitaiStructures
     @_io.read(4).unpack('V')[0]
   end
 
-  def read_u8le
-    @_io.read(8).unpack('Q')[0]
+  unless @@big_endian
+    def read_u8le
+      @_io.read(8).unpack('Q')[0]
+    end
+  else
+    def read_u8le
+      a, b = @_io.read(8).unpack('VV')
+      (b << 32) + a
+    end
   end
 
   def read_u2be
@@ -32,6 +42,17 @@ module KaitaiStructures
 
   def read_u4be
     @_io.read(4).unpack('N')[0]
+  end
+
+  if @@big_endian
+    def read_u8be
+      @_io.read(8).unpack('Q')[0]
+    end
+  else
+    def read_u8be
+      a, b = @_io.read(8).unpack('NN')
+      (a << 32) + b
+    end
   end
 
   # ========================================================================
@@ -43,15 +64,21 @@ module KaitaiStructures
   end
 
   def read_s2le
-    to_signed(@_io.read(2).unpack('v')[0], SIGN_MASK_16)
+    to_signed(read_u2le, SIGN_MASK_16)
   end
 
   def read_s4le
-    to_signed(@_io.read(4).unpack('V')[0], SIGN_MASK_32)
+    to_signed(read_u4le, SIGN_MASK_32)
   end
 
-  def read_s8le
-    @_io.read(8).unpack('q')[0]
+  unless @@big_endian
+    def read_s8le
+      @_io.read(8).unpack('q')[0]
+    end
+  else
+    def read_s8le
+      to_signed(read_u8le, SIGN_MASK_64)
+    end
   end
 
   def read_s2be
@@ -60,6 +87,16 @@ module KaitaiStructures
 
   def read_s4be
     @_io.read(4).unpack('N')[0]
+  end
+
+  if @@big_endian
+    def read_s8be
+      @_io.read(8).unpack('q')[0]
+    end
+  else
+    def read_s8be
+      to_signed(read_u8be, SIGN_MASK_64)
+    end
   end
 
   # ========================================================================
