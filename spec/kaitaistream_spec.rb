d30921abde482de68bc98be84375492777aff973
Mikhail Yakshin <greycat@altlinux.org>
2022-07-31 21:25:46 +0100

    Added test that shows that KaitaiStream can wrap TCPSocket

--- spec/kaitaistream_spec.rb
+++ spec/kaitaistream_spec.rb
@@ -1,5 +1,6 @@
 require 'kaitai/struct/struct'
 require 'stringio'
+require 'socket'
 require 'fileutils'
 
 RSpec.describe Kaitai::Struct::Stream do
@@ -24,11 +25,40 @@ RSpec.describe Kaitai::Struct::Stream do
       stream = Kaitai::Struct::Stream.new(io)
     end
 
-    it 'can be initialized from IO' do
+    it 'can be initialized from File' do
       File.binwrite('test12345.bin', '12345')
       File.open('test12345.bin', 'r') { |f|
         stream = Kaitai::Struct::Stream.new(f)
       }
+    end
+
+    it 'can be initialized from TCPSocket' do
+      HOST = '127.0.0.1'
+      PORT = 26570
+
+      # Start a new TCP server on designated port. This server will only accept one connection and then will cease to listen.
+      server = TCPServer.new(HOST, PORT)
+
+      # Run `accept` in a separate thread (as we still need the main thread for client-to-server connection)
+      Thread.start do
+        s2c_socket = server.accept
+        s2c_socket.write('12345')
+        s2c_socket.close
+        server.close
+      end
+
+      # Start client-to-server connection
+      c2s_socket = TCPSocket.new(HOST, PORT)
+
+      stream = Kaitai::Struct::Stream.new(c2s_socket)
+
+      # Check that we can read 1 byte integer from a socket
+      expect(stream.read_u1).to eq(0x31)
+
+      # Check that we can't seek in a socket IO
+      expect { stream.seek(2) }.to raise_error(Errno::ESPIPE)
+
+      c2s_socket.close
     end
 
     it 'cannot be initialized from an integer' do
